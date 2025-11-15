0a2527de648fa201f02d28cdbf5dded60247ad83
ru6by <kubus.rudzik@gmail.com>
2025-11-15 22:02:58 +0100

    Added Homotopy of paths definition Added lemma of R^n being simply connected

--- Hatcher.lean
+++ Hatcher.lean
@@ -1,13 +1,10 @@
-import Mathlib.Topology.Basic
-import Mathlib.Topology.UnitInterval
+import Mathlib.Topology.Path
 
-open TopologicalSpace
 open unitInterval
 
 variable {X Y : Type} [TopologicalSpace X] [TopologicalSpace Y]
 
 -- Definition 1 (Homotopy of maps)
-
 -- Main structure of homotopy
 structure Homotopy (f g : X → Y) where
   (H : X × I → Y)
@@ -45,3 +42,22 @@ example : ex_f ≃ ex_g := by
     leftH := exleftH
     rightH := exrightH
   }⟩
+
+-- Definition 2 (Path)
+-- We can take it from mathlib! (We also could use the definition of homotopy from Mathlib)
+
+-- Definition 3 (Loop)
+structure Loop (x : X) extends Path x x
+
+-- Definition 4 (Homotopy of paths)
+structure PathHomotopy (x y : X) (γ₁ γ₂ : Path x y) extends Homotopy γ₁ γ₂ where
+  (statH : ∀ (t : I), H (0, t) = x ∧ H (1,t) = y)
+
+def PathHomotopic (x y : X) (γ₁ γ₂ : Path x y) :=
+  Nonempty (PathHomotopy x y γ₁ γ₂)
+
+notation:50 f "≃ₚ" g => Homotopic f g
+
+-- Lemma 5 (All paths from to in are Homotopic)
+lemma Rn_allpathHomotopic (n : ℕ) :  ∀ (x y : Fin n → ℝ) (γ₁ γ₂ : Path x y), γ₁ ≃ₚ γ₂ := by
+  sorry
